58fb4434edb60f57daa4d8f61050412486f54c4c
wkempa1990 <wkempinski@outlook.com>
2020-01-19 15:39:31 +0100

    nowy projekt

--- src/Flaga.java
+++ src/Flaga.java
@@ -2,11 +2,12 @@ public class Flaga {
     public static void main(String[] args) {
         String a = "* * * * * * ==================================";
         String b = "==============================================";
+        String c =" * * * * *  ==================================";
         for (int i = 0; i < 4; i++) {
             System.out.println(a);
-            System.out.println(" * * * * *  ==================================");
+            System.out.println(c);
         }
-        System.out.println("* * * * * * ==================================");
+        System.out.println(a);
         for (int i = 0; i < 6; i++) {
             System.out.println(b);
         }
