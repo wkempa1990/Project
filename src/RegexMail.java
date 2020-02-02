c4df99416adadd4cde55bbfd3ad982bc7edc15a2
wkempa1990 <wkempinski@outlook.com>
2020-02-02 15:17:28 +0100

    nowy projekt

--- src/RegexMail.java
+++ src/RegexMail.java
@@ -2,12 +2,20 @@ import java.util.Scanner;
 import java.util.regex.Pattern;
 
 public class RegexMail {
+    private static String haslo = "wojtek";
     public static void main(String[] args) {
         System.out.println("Podaj maila : ");
         String a = new Scanner(System.in).nextLine();
         boolean matches = Pattern.matches("([A-Za-z]+)@([A-Za-z]+)((\\.([A-Za-z]{2,3})){1,2})", a);
         if (matches == true) {
             System.out.println("Podano poprawnego maila : " + a);
+            System.out.println("Podaj haslo: ");
+            String podajHaslo= new Scanner(System.in).nextLine();
+            if(haslo.equals(podajHaslo)){
+                System.out.println("Haslo poprawne :)");
+            }else{
+                System.out.println("Bledne haslo :(");
+            }
         } else {
             System.out.println("Podano niepoprawnego maila : " + a);
         }
