563935c108cdb6b6af67e5a335086ea2fb94d3a4
wkempa1990 <wkempinski@outlook.com>
2020-02-10 09:58:41 +0100

    nowy projekt

---
+++ src/TicTacToe/App.java
@@ -0,0 +1,9 @@
+package TicTacToe;
+
+public class App {
+    public static void main(String[] args) {
+    // Można zmieniac graczy z komputerowego na recznego.
+        Gra gra = new Gra(false, true);
+        gra.uruchom();
+    }
+}
